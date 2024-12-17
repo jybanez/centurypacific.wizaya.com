7c1460c327be04c12f47ebf064923da6a057ebfc
Jonathan Ybanez <j.ybanez@gmail.com>
2024-12-17 23:54:27 +0800

    Updates

--- www/components/com_engine/assets/js/common.js
+++ www/components/com_engine/assets/js/common.js
@@ -142,7 +142,6 @@
 	$extend(ENGINE,{
 		$overlayCount:0,
 		showOverlay:function(message,params){
-			return;
 			ENGINE.$overlayCount++;
 			var check = window.document.body.getElement('.engineOverlay');
 			if ($defined(check)) {
@@ -155,7 +154,6 @@
 			}).set('html','<div>'+message+'</div>');
 		},
 		hideOverlay:function(){
-			return;
 			ENGINE.$overlayCount--;
 			if (!ENGINE.$overlayCount) {
 				if ($defined(ENGINE.$overlay)){
